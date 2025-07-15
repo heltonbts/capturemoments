9c263b76057c23052806153e2139ea51df6d7c9e
Helton Batista <=>
2025-07-15 13:37:58 -0300

    feat: user create router

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,47 @@
-import fastify from 'fastify';
+import fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import db from './lib/prisma';
 
 const app = fastify({
   logger: true,
 });
 
 const start = async () => {
-  app.get('/', async (request, reply) => {
-    reply.send({
+  app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    reply.status(200).send({
       message: 'Hello, World!',
     });
   });
+
+  app.post(
+    '/create-account',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const { fullName, email, password } = request.body as {
+        fullName: string;
+        email: string;
+        password: string;
+      };
+
+      if (!fullName || !email || !password) {
+        reply.status(400).send({
+          message: 'Todos os campos são obrigatórios',
+        });
+      }
+
+      const isUser = await db.user.findFirst({
+        where: {
+          email: email,
+        },
+      });
+
+      if (isUser) {
+        reply.status(400).send({
+          error: true,
+          message: 'Usuário já cadastrado',
+        });
+      }
+    },
+  );
+
   app.listen({ port: 1311 }, () => {
     console.log('Server is UP!');
   });
