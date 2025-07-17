782814872487dc4253e2f916fdfbb3a970d46bb9
Helton Batista <heltonbts@icloud.com>
2025-07-17 10:16:08 -0300

    feat: login router is up, with verifications

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import db from './lib/prisma';
-import { hashSync } from 'bcrypt-ts';
+import { compare, hashSync } from 'bcrypt-ts';
 import jwt from 'jsonwebtoken';
 
 const app = fastify({
@@ -71,6 +71,58 @@ const start = async () => {
       });
     },
   );
+
+  app.post('/login', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { email, password } = request.body as {
+      email: string;
+      password: string;
+    };
+
+    if (!email || !password) {
+      reply.status(400).send({
+        message: 'Todos os campos são obrigatórios',
+      });
+    }
+
+    const isUser = await db.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!isUser) {
+      return reply.status(400).send({
+        error: true,
+        message: 'Usuário não encontrado, por favor, crie uma conta primeiro',
+      });
+    }
+
+    const isPasswordValid = await compare(password, isUser.password);
+    if (!isPasswordValid) {
+      return reply.status(400).send({
+        error: true,
+        message: 'Senha incorreta.',
+      });
+    }
+
+    const acessToken = await jwt.sign(
+      { userId: isUser.id },
+      process.env.ACCESS_TOKEN_SECRET!,
+      {
+        expiresIn: '72h',
+      },
+    );
+
+    return reply.status(200).send({
+      error: false,
+      user: {
+        fullName: isUser.fullName,
+        email: isUser.email,
+      },
+      acessToken,
+      message: 'Login realizado com sucesso',
+    });
+  });
 
   app.listen({ port: 1311 }, () => {
     console.log('Server is UP!');
