0ca2b6ecc23c82f03720e339b3bfe891791b8eaf
Helton Batista <heltonbts@icloud.com>
2025-07-18 12:56:48 -0300

    refactor:

---
+++ src/controller/getAllRegisterController.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { GetAllRegisterService } from '../services/getAllRegisterService';
+
+class getAllRegisterController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { user } = request;
+
+    if (!user) {
+      throw new Error('Usuário não existe');
+    }
+
+    try {
+      const getAllRegister = await new GetAllRegisterService().execute({
+        userId: user.userId,
+      });
+
+      return reply
+        .send({
+          moments: getAllRegister,
+          message: 'registro obtidos com sucesso',
+        })
+        .status(200);
+    } catch (error: any) {
+      return reply.status(400).send({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+}
+
+export { getAllRegisterController };
