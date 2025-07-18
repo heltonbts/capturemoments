487b9c82e80049243757820d62483d1f85af4091
Helton Batista <heltonbts@icloud.com>
2025-07-18 10:16:38 -0300

    refactor: move middleware for new folder

--- src/services/auth/GetUsersService.ts
+++ src/services/auth/GetUsersService.ts
@@ -1,35 +1,4 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-import jwt from 'jsonwebtoken';
 import db from '../../lib/prisma';
-
-export async function authenticateToken(
-  request: FastifyRequest,
-  reply: FastifyReply,
-) {
-  const authHeader = request.headers['authorization'];
-  const token = authHeader?.split(' ')[1];
-
-  if (!token) {
-    return reply
-      .status(400)
-      .send({ error: true, message: 'token not provided' });
-  }
-
-  try {
-    const decoded = (await jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET!,
-    )) as {
-      userId: string;
-    };
-    request.user = { db, userId: decoded.userId };
-  } catch (error: any) {
-    return reply.status(400).send({
-      error: true,
-      message: error.message,
-    });
-  }
-}
 
 interface UserRequest {
   userId: string;
